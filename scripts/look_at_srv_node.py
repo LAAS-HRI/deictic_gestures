a40806b3dff476a2e9c6423d7f33eb9c90f15a0b
Yoan Sallami <ysallami@laas.fr>
2018-04-06 16:27:40 +0200

    add production of uwds moving situation

--- scripts/look_at_srv_node.py
+++ scripts/look_at_srv_node.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 
+import time
 import rospy
 import tf
-from std_srvs.srv import *
-from geometry_msgs.msg import *
-from nao_interaction_msgs.srv import *
-from deictic_gestures.srv import *
+import sys
+import argparse
+from std_msgs.msg import String
+from std_srvs.srv import Empty
+from geometry_msgs.msg import Point, PointStamped
+from nao_interaction_msgs.srv import TrackerLookAt
+from deictic_gestures.srv import LookAt
+import underworlds
+from underworlds.types import Situation
 
 LOOK_AT_MAX_SPEED = 0.6
 
 class LookAtSrv(object):
-    def __init__(self):
+    def __init__(self, ctx, world):
+        self.world = ctx.worlds[world]
         rospy.loginfo("waiting for service /naoqi_driver/tracker/look_at")
         rospy.wait_for_service("/naoqi_driver/tracker/look_at")
         rospy.loginfo("waiting for service /naoqi_driver/tracker/stop_tracker")
@@ -19,8 +26,7 @@ class LookAtSrv(object):
             "look_at": rospy.ServiceProxy("naoqi_driver/tracker/look_at", TrackerLookAt),
             "stop_tracker": rospy.ServiceProxy("/naoqi_driver/tracker/stop_tracker", Empty)}
 
-        self.services = {"look_at": rospy.Service('/deictic_gestures/look_at', LookAt,
-                                                   self.handle_look_at)}
+        self.services = {"look_at": rospy.Service('/deictic_gestures/look_at', LookAt, self.handle_look_at)}
 
         self.tfListener = tf.TransformListener()
         self.parameters = {"fixed_frame": rospy.get_param("global_frame_id", "/map"),
@@ -28,7 +34,31 @@ class LookAtSrv(object):
                            "look_at_max_speed": rospy.get_param("look_at_max_speed", LOOK_AT_MAX_SPEED)}
 
         self.publishers = {
-            "result_point": rospy.Publisher('/deictic_gestures/looking_point', geometry_msgs.msg.PointStamped, queue_size=5)}
+            "result_point": rospy.Publisher('/deictic_gestures/looking_point', PointStamped, queue_size=5)}
+
+        self.log_pub = {"situation_log": rospy.Publisher("look_at_srv/log", String, queue_size=5)}
+
+        self.current_situations_map = {}
+
+    def start_n1_situation(self, timeline, predicate, subject_name, isevent=False):
+        description = predicate + "(" + subject_name + ")"
+        sit = Situation(desc=description)
+        sit.starttime = time.time()
+        if isevent:
+            sit.endtime = sit.starttime
+        self.current_situations_map[description] = sit
+        self.log_pub["situation_log"].publish("START " + description)
+        timeline.update(sit)
+        return sit.id
+
+    def end_n1_situation(self, timeline, predicate, subject_name):
+        description = predicate + "(" + subject_name + ")"
+        sit = self.current_situations_map[description]
+        self.log_pub["situation_log"].publish("END " + description)
+        try:
+            timeline.end(sit)
+        except Exception as e:
+            rospy.logwarn("[robot_monitor] Exception occurred : " + str(e))
 
     def handle_look_at(self, req):
         # First version using naoqi
@@ -40,16 +70,26 @@ class LookAtSrv(object):
             req.point.point.y += translation[1]
             req.point.point.z += translation[2]
             self.services_proxy["stop_tracker"]()
-            target = Point(req.point.point.x, req.point.point.y, req.point.point.z)
-            self.services_proxy["look_at"](target, 0, LOOK_AT_MAX_SPEED, 0)
             self.publishers["result_point"].publish(req.point)
+            target = Point(req.point.point.x, req.point.point.y, req.point.point.z)
+            self.start_n1_situation(self.world.timeline, "moving", "robot")
+            self.services_proxy["look_at"](target, 0, LOOK_AT_MAX_SPEED, 0)
+            self.end_n1_situation(self.world.timeline, "moving", "robot")
             return True
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException), e:
             rospy.logerr("[look_at_srv] Exception occurred :" + str(e))
             return False
 
 if __name__ == '__main__':
+    sys.argv = [arg for arg in sys.argv if "__name" not in arg and "__log" not in arg]
+    sys.argc = len(sys.argv)
+
+    parser = argparse.ArgumentParser(description="Handle look at")
+    parser.add_argument("world", help="The world where to write the situation associated to moving")
+    args = parser.parse_args()
+
     rospy.init_node('look_at_srv')
-    srv = LookAtSrv()
-    rospy.spin()
-    exit(0)
+    with underworlds.Context("look_at_srv") as ctx:  # Here we connect to the server
+        LookAtSrv(ctx, args.world)
+        rospy.spin()
+        exit(0)
--- scripts/point_at_srv_node.py
+++ scripts/point_at_srv_node.py
@@ -2,16 +2,22 @@
 
 import rospy
 import tf
-from std_srvs.srv import *
-
-from geometry_msgs.msg import *
-from nao_interaction_msgs.srv import *
-from deictic_gestures.srv import *
+import sys
+import time
+import argparse
+from std_msgs.msg import String
+from std_srvs.srv import Empty
+from geometry_msgs.msg import PointStamped, Point
+from nao_interaction_msgs.srv import TrackerPointAt
+from deictic_gestures.srv import PointAt
+import underworlds
+from underworlds.types import Situation
 
 POINT_AT_MAX_SPEED = 0.6
 
 class PointAtSrv(object):
-    def __init__(self):
+    def __init__(self, ctx, world):
+        self.world = ctx.worlds[world]
         rospy.loginfo("waiting for service /naoqi_driver/tracker/point_at")
         rospy.wait_for_service("/naoqi_driver/tracker/point_at")
         self.services_proxy = {
@@ -26,7 +32,31 @@ class PointAtSrv(object):
                            "point_at_max_speed": rospy.get_param("point_at_max_speed", POINT_AT_MAX_SPEED)}
 
         self.publishers = {
-            "result_point": rospy.Publisher('/deictic_gestures/pointing_point', geometry_msgs.msg.PointStamped, queue_size=5)}
+            "result_point": rospy.Publisher('/deictic_gestures/pointing_point', PointStamped, queue_size=5)}
+
+        self.log_pub = {"situation_log": rospy.Publisher("look_at_srv/log", String, queue_size=5)}
+
+        self.current_situations_map = {}
+
+    def start_n1_situation(self, timeline, predicate, subject_name, isevent=False):
+        description = predicate + "(" + subject_name + ")"
+        sit = Situation(desc=description)
+        sit.starttime = time.time()
+        if isevent:
+            sit.endtime = sit.starttime
+        self.current_situations_map[description] = sit
+        self.log_pub["situation_log"].publish("START " + description)
+        timeline.update(sit)
+        return sit.id
+
+    def end_n1_situation(self, timeline, predicate, subject_name):
+        description = predicate + "(" + subject_name + ")"
+        sit = self.current_situations_map[description]
+        self.log_pub["situation_log"].publish("END " + description)
+        try:
+            timeline.end(sit)
+        except Exception as e:
+            rospy.logwarn("[robot_monitor] Exception occurred : " + str(e))
 
     def handle_point_at(self, req):
         # First version using naoqi
@@ -40,7 +70,9 @@ class PointAtSrv(object):
             req.point.point.z += translation[2]
             effector = "LArm" if req.point.point.y > 0.0 else "RArm"
             target = Point(req.point.point.x, req.point.point.y, req.point.point.z)
+            self.start_n1_situation(self.world.timeline, "moving", "robot")
             self.services_proxy["point_at"](effector, target, 0, POINT_AT_MAX_SPEED)
+            self.end_n1_situation(self.world.timeline, "moving", "robot")
             self.publishers["result_point"].publish(req.point)
             return True
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException), e:
@@ -49,7 +81,15 @@ class PointAtSrv(object):
 
 
 if __name__ == '__main__':
+    sys.argv = [arg for arg in sys.argv if "__name" not in arg and "__log" not in arg]
+    sys.argc = len(sys.argv)
+
+    parser = argparse.ArgumentParser(description="Handle look at")
+    parser.add_argument("world", help="The world where to write the situation associated to moving")
+    args = parser.parse_args()
+
     rospy.init_node('point_at_srv')
-    srv = PointAtSrv()
-    rospy.spin()
-    exit(0)
+    with underworlds.Context("uwds_database_ros_bridge") as ctx:  # Here we connect to the server
+        PointAtSrv(ctx, args.world)
+        rospy.spin()
+        exit(0)
