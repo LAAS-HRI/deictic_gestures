9fd96c26b7dddb6d659e9651dbc0cd01a5476668
guilhembn <gbuisan@laas.fr>
2019-06-19 11:02:47 +0200

    Fixes lookAt

--- scripts/look_at_srv_node.py
+++ scripts/look_at_srv_node.py
@@ -12,14 +12,25 @@ from geometry_msgs.msg import Point, PointStamped
 from naoqi import ALProxy
 from deictic_gestures.srv import LookAt, CanLookAt
 from std_srvs.srv import SetBool
-import underworlds
-from underworlds.types import Situation
-from underworlds.helpers.transformations import translation_matrix, quaternion_matrix
+from pepper_resources_synchronizer_msgs.srv import MetaStateMachineRegister, MetaStateMachineRegisterRequest
+from pepper_resources_synchronizer_msgs.msg import SubStateMachine_pepper_arm_manager_msgs, SubStateMachine_pepper_head_manager_msgs, SubStateMachine_pepper_base_manager_msgs
+from pepper_head_manager_msgs.msg import StateMachineStatePrioritizedPoint as HeadStatePoint
+from pepper_base_manager_msgs.msg import StateMachineStatePrioritizedAngle as BaseStateAngle
+from resource_management_msgs.msg import StateMachineTransition, MessagePriority
+
+from tf.transformations import translation_matrix, quaternion_matrix, quaternion_from_euler
 
 LOOK_AT_MAX_SPEED = 0.3
 LOOK_AT_SPEED = 0.1
-LOOK_AT_MAX_ANGLE = 90
+LOOK_AT_MAX_ANGLE = math.radians(80)
 MIN_DIST_MOVE = 0.1
+
+def center_radians(a):
+    while a >= math.pi:
+        a -= 2*math.pi
+    while a < -math.pi:
+        a += 2*math.pi
+    return a
 
 def transformation_matrix(t, q):
     translation_mat = translation_matrix(t)
@@ -28,17 +39,12 @@ def transformation_matrix(t, q):
 
 
 class LookAtSrv(object):
-    def __init__(self, ctx, world, nao_ip, nao_port):
-        self.world = ctx.worlds[world]
-        self.nao_ip = nao_ip
-        self.nao_port = nao_port
-        self.tracker = ALProxy("ALTracker", nao_ip, nao_port)
-        self.motion = ALProxy("ALMotion", nao_ip, nao_port)
+    def __init__(self):
 
         self.services = {"look_at": rospy.Service('/deictic_gestures/look_at', LookAt, self.handle_look_at),
                          "can_look_at": rospy.Service('/deictic_gestures/can_look_at', CanLookAt, self.handle_can_look_at)}
 
-        self.services_proxy = {"enable_monitoring": rospy.ServiceProxy('multimodal_human_monitor/global_monitoring', SetBool)}
+        #self.services_proxy = {"enable_monitoring": rospy.ServiceProxy('multimodal_human_monitor/global_monitoring', SetBool)}
 
         self.tfListener = tf.TransformListener()
         self.parameters = {"fixed_frame": rospy.get_param("global_frame_id", "/map"),
@@ -48,39 +54,16 @@ class LookAtSrv(object):
         self.publishers = {
             "result_point": rospy.Publisher('/deictic_gestures/looking_point', PointStamped, queue_size=5)}
 
-        self.log_pub = {"isLookingAt": rospy.Publisher("predicates_log/lookingat", String, queue_size=5),
-                        "isMoving": rospy.Publisher("predicates_log/moving", String, queue_size=5)}
+        #self.log_pub = {"isLookingAt": rospy.Publisher("predicates_log/lookingat", String, queue_size=5),
+        #                "isMoving": rospy.Publisher("predicates_log/moving", String, queue_size=5)}
 
-        self.current_situations_map = {}
+        #self.current_situations_map = {}
+
+        self.resource_synchronizer = rospy.ServiceProxy("/pepper_resources_synchronizer/state_machines_register", MetaStateMachineRegister)
+
 
         self.current_lookat_frame = None
         self.current_lookat_point = None
-
-    def start_predicate(self, timeline, predicate, subject_name, object_name=None, isevent=False):
-        if object_name is None:
-            description = predicate + "(" + subject_name + ")"
-        else:
-            description = predicate + "(" + subject_name + "," + object_name + ")"
-        sit = Situation(desc=description)
-        sit.starttime = time.time()
-        if isevent:
-            sit.endtime = sit.starttime
-        self.current_situations_map[description] = sit
-        self.log_pub[predicate].publish("START " + description)
-        timeline.update(sit)
-        return sit.id
-
-    def end_predicate(self, timeline, predicate, subject_name, object_name=None):
-        if object_name is None:
-            description = predicate + "(" + subject_name + ")"
-        else:
-            description = predicate + "(" + subject_name + "," + object_name + ")"
-        try:
-            sit = self.current_situations_map[description]
-            self.log_pub[predicate].publish("END " + description)
-            timeline.end(sit)
-        except Exception as e:
-            rospy.logwarn("[look_at_srv] Exception occurred : " + str(e))
 
     def distance(self, point):
         x = point[0]-self.current_lookat_point[0]
@@ -96,7 +79,7 @@ class LookAtSrv(object):
                 p = numpy.atleast_2d([req.point.point.x, req.point.point.y, req.point.point.z, 1]).transpose()
                 new_p = numpy.dot(t, p)
                 angle = math.atan2(new_p[1, 0], new_p[0, 0])
-                if math.degrees(math.fabs(angle)) > LOOK_AT_MAX_ANGLE:
+                if math.fabs(angle) > LOOK_AT_MAX_ANGLE:
                     if angle > 0:
                         angle += 0.1
                     else:
@@ -109,7 +92,6 @@ class LookAtSrv(object):
             return False, 0
 
     def handle_look_at(self, req, ):
-        # First version using naoqi
         self.parameters["look_at_max_speed"] = rospy.get_param("look_at_max_speed", LOOK_AT_MAX_SPEED)
         try:
             if self.tfListener.canTransform("/torso", req.point.header.frame_id, rospy.Time()):
@@ -121,7 +103,7 @@ class LookAtSrv(object):
                 dist = None
                 speed_up = False
                 if self.current_lookat_point:
-                    dist = self.distance([new_p[1, 0], new_p[2, 0], new_p[3, 0]])
+                    dist = self.distance([new_p[0, 0], new_p[1, 0], new_p[2, 0]])
                     if dist > MIN_DIST_MOVE or self.current_lookat_frame != req.point.header.frame_id:
                         to_move = True
 
@@ -131,19 +113,11 @@ class LookAtSrv(object):
                     speed_up = True if dist > 0.35 else False
 
                 if to_move:
-                    (translation, rotation) = self.tfListener.lookupTransform('/torso', req.point.header.frame_id,
+                    (translation, rotation) = self.tfListener.lookupTransform('torso', req.point.header.frame_id,
                                                                               rospy.Time())
                     t = transformation_matrix(translation, rotation)
                     p = numpy.atleast_2d([req.point.point.x, req.point.point.y, req.point.point.z, 1]).transpose()
                     new_p = numpy.dot(t, p)
-                    if req.point.header.frame_id != self.current_lookat_frame:
-                        if self.current_lookat_frame is not None:
-                            self.end_predicate(self.world.timeline, "isLookingAt", "robot",
-                                               object_name=self.current_lookat_frame)
-                        self.start_predicate(self.world.timeline, "isLookingAt", "robot",
-                                             object_name=req.point.header.frame_id)
-                        self.current_lookat_frame = req.point.header.frame_id
-                    self.start_predicate(self.world.timeline, "isMoving", "robot")
                     #self.tracker.stopTracker()
                     #self.publishers["result_point"].publish()
                     #rospy.logwarn("lookat")
@@ -152,19 +126,108 @@ class LookAtSrv(object):
                         look_at_speed = LOOK_AT_MAX_SPEED
                     else:
                         look_at_speed = LOOK_AT_SPEED
-                    self.current_lookat_point = [new_p[1, 0], new_p[2, 0], new_p[3, 0]]
-                    try:
-                        self.services_proxy["enable_monitoring"](False)
-                        self.tracker.lookAt([new_p[0, 0], new_p[1, 0], new_p[2, 0]], look_at_speed, False)
-                        time.sleep(0.1)
-                        self.services_proxy["enable_monitoring"](True)
-                    except Exception:
-                        self.tracker = ALProxy("ALTracker", self.nao_ip, self.nao_port)
-                        self.services_proxy["enable_monitoring"](False)
-                        self.tracker.lookAt([new_p[0, 0], new_p[1, 0], new_p[2, 0]], look_at_speed, False)
-                        time.sleep(0.1)
-                        self.services_proxy["enable_monitoring"](True)
-                    self.end_predicate(self.world.timeline, "isMoving", "robot")
+                    self.current_lookat_point = [new_p[0, 0], new_p[1, 0], new_p[2, 0]]
+                    # try:
+                    #     self.services_proxy["enable_monitoring"](False)
+                    #     self.tracker.lookAt([new_p[0, 0], new_p[1, 0], new_p[2, 0]], look_at_speed, False)
+                    #     time.sleep(0.1)
+                    #     self.services_proxy["enable_monitoring"](True)
+                    # except Exception:
+                    #     self.tracker = ALProxy("ALTracker", self.nao_ip, self.nao_port)
+                    #     self.services_proxy["enable_monitoring"](False)
+                    #     self.tracker.lookAt([new_p[0, 0], new_p[1, 0], new_p[2, 0]], look_at_speed, False)
+                    #     time.sleep(0.1)
+                    #     self.services_proxy["enable_monitoring"](True)
+                    # self.end_predicate(self.world.timeline, "isMoving", "robot")
+
+                    angle = math.atan2(new_p[1, 0], new_p[0, 0])
+                    angle_diff = 0.0
+                    if math.fabs(angle) > LOOK_AT_MAX_ANGLE:
+                        if not req.with_base:
+                            return False
+                        angle_diff = min(center_radians(angle - LOOK_AT_MAX_ANGLE), center_radians(angle + LOOK_AT_MAX_ANGLE), key=lambda x: abs(x))
+
+                    if angle_diff != 0:
+                        rospy.loginfo("Will turn: " + str(math.degrees(angle_diff)))
+
+                    after_rot = transformation_matrix([0, 0, 0], quaternion_from_euler(0, 0, -angle_diff))
+                    new_p = numpy.dot(after_rot, new_p)
+                    rospy.loginfo("Point after rotation: " + str(new_p))
+
+                    mfsm = MetaStateMachineRegisterRequest()
+                    headfsm = SubStateMachine_pepper_head_manager_msgs()
+                    basefsm = SubStateMachine_pepper_base_manager_msgs()
+                    head_wait_turned = HeadStatePoint()
+                    head_wait_turned.header.id = "_head_wait_turned"
+                    base_turn = BaseStateAngle()
+                    base_turn.data = angle_diff
+                    base_turn.header.id = "base_turn"
+                    head_look = HeadStatePoint()
+                    head_look.header.id = "look"
+                    head_look.data.header.frame_id = "torso"
+                    head_look.data.header.stamp = rospy.Time.now()
+                    head_look.data.point.x, head_look.data.point.y, head_look.data.point.z = new_p[0, 0], new_p[1, 0], new_p[2, 0]
+                    base_idle = BaseStateAngle()
+                    base_idle.header.id = "_base_idle"
+                    head_end = HeadStatePoint()
+                    head_end.header.id = "head_end"
+                    head_end.data = head_look.data
+                    t_head_wait_turn = StateMachineTransition()
+                    t_head_wait_turn.next_state = "look"
+                    t_head_wait_turn.end_condition.duration = rospy.Duration(-1)
+                    t_head_wait_turn.end_condition.timeout = rospy.Duration(-1)
+                    t_head_wait_turn.end_condition.regex_end_condition.append("__synchro__turned")
+                    head_wait_turned.header.transitions.append(t_head_wait_turn)
+                    t_base_turn = StateMachineTransition()
+                    t_base_turn.next_state = "_base_idle"
+                    t_base_turn.end_condition.timeout = rospy.Duration(-1)
+                    t_base_turn.end_condition.duration = rospy.Duration(-1)
+                    t_base_turn.end_condition.regex_end_condition.append("__synchro__turned")
+                    base_turn.header.transitions.append(t_base_turn)
+                    t_head_look = StateMachineTransition()
+                    t_head_look.end_condition.duration = rospy.Duration(5)
+                    t_head_look.end_condition.timeout = rospy.Duration(-1)
+                    t_head_look.next_state = "head_end"
+                    head_look.header.transitions.append(t_head_look)
+                    t_head_end = StateMachineTransition()
+                    t_head_end.next_state = "end"
+                    t_head_end.end_condition.timeout = rospy.Duration(-1)
+                    t_head_end.end_condition.duration = rospy.Duration(-1)
+                    t_head_end.end_condition.regex_end_condition.append("__synchro__end")
+                    head_end.header.transitions.append(t_head_end)
+                    t_base_end = StateMachineTransition()
+                    t_base_end.next_state = "end"
+                    t_base_end.end_condition.duration = rospy.Duration(-1)
+                    t_base_end.end_condition.timeout = rospy.Duration(-1)
+                    t_base_end.end_condition.regex_end_condition.append("__synchro__end")
+                    base_idle.header.transitions.append(t_base_end)
+
+                    headfsm.state_machine.states_PrioritizedPoint.append(head_wait_turned)
+                    headfsm.state_machine.states_PrioritizedPoint.append(head_look)
+                    headfsm.state_machine.states_PrioritizedPoint.append(head_end)
+                    basefsm.state_machine.states_PrioritizedAngle.append(base_turn)
+                    basefsm.state_machine.states_PrioritizedAngle.append(base_idle)
+                    if angle_diff != 0.0:
+                        headfsm.header.initial_state = "_head_wait_turned"
+                        basefsm.header.initial_state = "base_turn"
+                    else:
+                        headfsm.header.initial_state = "look"
+                        basefsm.header.initial_state = "_base_idle"
+                    headfsm.header.timeout = rospy.Duration(-1)
+                    headfsm.header.begin_dead_line = rospy.Time.now() + rospy.Duration(5)
+                    basefsm.header.begin_dead_line = rospy.Time.now() + rospy.Duration(5)
+                    basefsm.header.timeout = rospy.Duration(-1)
+
+                    mfsm.state_machine_pepper_head_manager = headfsm
+                    mfsm.state_machine_pepper_base_manager = basefsm
+                    mfsm.header.timeout = rospy.Duration(-1)
+                    mfsm.header.begin_dead_line = rospy.Time.now() + rospy.Duration(5)
+                    mfsm.header.priority.value = MessagePriority.URGENT
+
+                    self.resource_synchronizer.call(mfsm)
+
+
+
                 return True
             else:
                 if self.current_lookat_frame is not None:
@@ -180,14 +243,7 @@ if __name__ == '__main__':
     sys.argv = [arg for arg in sys.argv if "__name" not in arg and "__log" not in arg]
     sys.argc = len(sys.argv)
 
-    parser = argparse.ArgumentParser(description="Handle look at")
-    parser.add_argument("world", help="The world where to write the situation associated to moving")
-    parser.add_argument("--nao_ip", default="mummer-eth0.laas.fr", help="The robot IP")
-    parser.add_argument("--nao_port", default="9559", help="The robot port")
-    args = parser.parse_args()
-
     rospy.init_node('look_at_srv')
-    with underworlds.Context("look_at_srv") as ctx:  # Here we connect to the server
-        LookAtSrv(ctx, args.world, args.nao_ip, int(args.nao_port))
-        rospy.spin()
-        exit(0)
+    l = LookAtSrv()
+    rospy.spin()
+    exit(0)
